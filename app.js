7142dcd419a3fd7fefe819fdc1fb7a8de288a2f1
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-04-21 20:29:17 -0300

    Configurando client do twitter

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const Twitter = require('twitter');
 
 const app = express();
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'hbs');
+
+// Configuração do twitter Client. 
+// As variáveis abaixo devem estar no arquivo '.env', na raiz deste projeto
+// como seguinte formato:
+// TWITTER_CONSUMER_KEY=(consumer_key da sua aplicação, fornecida pelo twitter)
+// TWITTER_CONSUMER_SECRET=(consumer_secret da sua aplicação)
+// TWITTER_ACCESS_TOKEN_KEY=(access_token_key da sua aplicação)
+// TWITTER_ACCESS_TOKEN_SECRET=(acces_token_secret da sua aplicação)
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 // Rotas públicas (acesso sem autenticação)
 app.get('/', (request, response) => {
