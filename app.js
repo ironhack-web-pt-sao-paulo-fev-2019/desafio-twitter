390025b481ccb7b75346f689284ec00a73365ce1
José Luiz Coe <joseluizcoe@Joses-MacBook-Pro.local>
2019-04-21 11:18:21 -0300

    Adicionando rotas publicas

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Rotas públicas (acesso sem autenticação)
+app.get('/', (request, response) => {
+  response.send('HOME: Esta é a rota principal, ela precisa renderizar um template com um form de busca');
+});
+
+app.get('/search-results', (request, response) => {
+  response.send('SEARCH RESULTS: Esta é a rota com o resultado da busca de acordo com os criterios escolhidos na capa do site.');
+});
+
+app.get('/user-detail/:username', (request, response) => {
+  response.send('user-detail: Esta é a rota com os tuwits de um determinado usuário.');
+});
+
+// Rotas privadas (acessa apenas para quem está autenticado)
+
+
+// Inicialização do servidor
 app.listen(3000, (error) => {
   if(error) {
     console.log(error);
