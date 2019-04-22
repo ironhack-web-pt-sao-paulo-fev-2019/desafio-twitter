e565d2ded2233d81373de5ee69acce9f9880f7b9
Sany Chernizon <chernizon@gmail.com>
2019-04-22 13:10:39 -0300

    Autorização funcionando

--- app.js
+++ app.js
@@ -2,22 +2,73 @@ const express = require('express'),
     app = express(),
     hbs = require('hbs'),
     Twitter = require('twitter'),
-    authRoutes = require('./routes/auth-routes');
-
+    session = require('express-session'),
+    passport = require('passport'),
+    TwitterStrategy = require('passport-twitter').Strategy;
 require('dotenv').config();
 
 // SET UP
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: 'blabla', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session())
 hbs.registerPartials(__dirname + '/views/partials');
 
-// SET UP ROUTES
-app.use('/auth', authRoutes);
+// SET UP PASSPORT
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+},
+    function (token, tokenSecret, profile, cb) {
+        console.log(profile);
+        return cb(null, profile);
+    }
+));
+
+passport.serializeUser(function (user, callback) {
+    callback(null, user);
+})
+
+passport.deserializeUser(function (obj, callback) {
+    callback(null, obj);
+})
+
+// SET UP PASSPORT ROUTES
+
+// auth login
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// auth logout
+app.get('/logout', (req, res) => {
+    //handle with passport
+    res.send('logging out');
+})
+
+// auth with twitter
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        // console.log(req.user)
+        var user = req.user
+        res.render('login', {user});
+    });
 
 // ROUTES
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 // ROUTES //TWITTER
